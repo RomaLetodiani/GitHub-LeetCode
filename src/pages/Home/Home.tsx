cdb4eff9c2790b8f8784b932e58cd0230898ffd3
RomaLetodiani <roman.letodiani03@gmail.com>
2024-05-21 18:01:38 +0400

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,21 @@
-const Home = () => {
-  return <div>Home</div>;
-};
+import { Link, Outlet } from 'react-router-dom'
+import Dashboard from '../../components/Dashboard'
 
-export default Home;
+const Home = () => {
+  return (
+    <div>
+      <div>
+        <h1>Home</h1>
+        <p>Welcome to the home page!</p>
+        <Link to={'leetcode'}>LeetCode</Link>
+        <Link to={'github'}>GitHub</Link>
+        <Dashboard />
+        <div className="bg-red-500">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
