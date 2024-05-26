efd90ac34433d199202e7de12a293f6c9574770e
RomaLetodiani <roman.letodiani03@gmail.com>
2024-05-26 17:51:29 +0400

    Update Home.tsx

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -1,18 +1,54 @@
-import { Link, Outlet } from 'react-router-dom'
-
+import { Outlet, useLocation, useNavigate } from 'react-router-dom'
+import { motion } from 'framer-motion'
+import Card from '../../components/Card'
+import useWindowSize from '../../hooks/useWindowSize'
+import { useEffect } from 'react'
+const transitionVariants = (dimensions: { height: number; width: number }) => ({
+  open: {
+    clipPath: `circle(${dimensions.height * 3}px at 0px 0px)`,
+    transition: {
+      delay: 0.2,
+      type: 'spring',
+      stiffness: 20,
+      restDelta: 2,
+    },
+  },
+  closed: {
+    clipPath: `circle(30px at ${dimensions.width / 2}px ${dimensions.height / 2}px)`,
+    transition: {
+      delay: 0.2,
+      type: 'spring',
+      stiffness: 400,
+      damping: 40,
+    },
+  },
+})
 const Home = () => {
+  const { pathname } = useLocation()
+  const navigate = useNavigate()
+  const isLeetCode = pathname.includes('leetcode')
+  const dimensions = useWindowSize()
+  useEffect(() => {
+    if (pathname === '/') {
+      navigate('/github')
+    }
+  }, [])
   return (
-    <div>
-      <div>
-        <h1>Home</h1>
-        <p>Welcome to the home page!</p>
-        <Link to={'leetcode'}>LeetCode</Link>
-        <Link to={'github'}>GitHub</Link>
-        <div className="bg-red-500">
-          <Outlet />
-        </div>
-      </div>
-    </div>
+    <motion.div
+      className="relative flex justify-center items-center inset-0 w-full h-screen bg-github-gradient"
+      initial={false}
+      animate={isLeetCode ? 'open' : 'closed'}
+      custom={dimensions}
+    >
+      <motion.div
+        className="fixed -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 w-full h-full bg-leetcode-gradient"
+        variants={transitionVariants(dimensions)}
+      />
+
+      <Card>
+        <Outlet />
+      </Card>
+    </motion.div>
   )
 }
 
