9521eaffb0a9158773b797cb98a8c7999ebb3816
RomaLetodiani <roman.letodiani03@gmail.com>
2024-05-29 15:37:47 +0400

    TansTack Query

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,8 +1,24 @@
 import { RouterProvider } from 'react-router-dom'
 import Router from './router/Router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 10,
+    },
+  },
+})
 
 const App = () => {
-  return <RouterProvider router={Router} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={Router} />
+    </QueryClientProvider>
+  )
 }
 
 export default App
