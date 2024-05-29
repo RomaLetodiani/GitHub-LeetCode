676b90e54b50e329a1a19173950499a4a8675668
RomaLetodiani <roman.letodiani03@gmail.com>
2024-05-29 18:00:42 +0400

    Create Avatar.tsx

---
+++ Frontend/src/components/Avatar.tsx
@@ -0,0 +1,21 @@
+type avatarProps = {
+  url: string
+  alt: string
+  className: string
+  userName: string
+  desc: string
+}
+
+const Avatar = ({ url, alt, className, userName, desc }: avatarProps) => {
+  return (
+    <div>
+      <img src={url} alt={alt} className={className} />
+      <div className="flex flex-col items-center">
+        <h1 className="text-3xl font-bold text-white">{userName}</h1>
+        <p className="text-white">{desc}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Avatar
