50d1057fae47230eef55bc9108c8bdd865a65c74
RomaLetodiani <roman.letodiani03@gmail.com>
2024-06-07 17:32:05 +0400

    Update Avatar.tsx

--- Frontend/src/components/Avatar.tsx
+++ Frontend/src/components/Avatar.tsx
@@ -1,18 +1,41 @@
+import { useQuery } from '@tanstack/react-query'
+import Loading from './Loading'
+
 type avatarProps = {
   url?: string
   alt?: string
   className: string
   userName?: string
   desc?: string
+  loading?: boolean
+  isError?: boolean
 }
 
-const Avatar = ({ url, alt, className, userName, desc }: avatarProps) => {
+const Avatar = ({
+  url,
+  alt,
+  className,
+  userName,
+  desc,
+  loading = false,
+  isError = false,
+}: avatarProps) => {
+  if (loading) {
+    return <Loading />
+  }
+
+  if (!url) {
+    return <div>Try Searching with the valid Username</div>
+  }
+
+  const { data: errorProfile } = useQuery({ queryKey: ['errorProfile'] })
+  console.log('🚀 ~ errorProfile:', errorProfile)
   return (
     <div>
       <img src={url} alt={alt} className={className} />
       <div className="flex flex-col items-center">
-        <h1 className="text-3xl font-bold text-white">{userName}</h1>
-        <p className="text-white">{desc}</p>
+        <h1 className="text-3xl font-bold">{userName}</h1>
+        <p className="">{desc}</p>
       </div>
     </div>
   )
